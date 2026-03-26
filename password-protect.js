c844366b11be14b3fc198adc260c598a3f0ca9bc
Floris Lieshout <florislieshout@Mac.fritz.box>
2026-03-26 16:14:56 +0100

    Add password protection for GitHub Pages

--- password-protect.js
+++ password-protect.js
@@ -1,9 +1,8 @@
 // Simple password protection for ADHD Support website
 (function() {
-    const correctPassword = "adhd2026"; // Change this to your preferred password
+    const correctPassword = "adhd2026";
     const passwordPrompt = "Enter password to access this site:";
     
-    // Check if user has already authenticated
     if (sessionStorage.getItem('authenticated') !== 'true') {
         const password = prompt(passwordPrompt);
         if (password === correctPassword) {
